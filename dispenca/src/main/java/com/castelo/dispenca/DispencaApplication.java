13a98c96477fe303ce2351dac08f7e850b1232b8
gydeaowj <w.gydeao@escola.pr.gov.br>
2024-08-14 12:09:24 -0300

    projeto inicial

---
+++ dispenca/src/main/java/com/castelo/dispenca/DispencaApplication.java
@@ -0,0 +1,13 @@
+package com.castelo.dispenca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DispencaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DispencaApplication.class, args);
+	}
+
+}
---
+++ dispenca/src/test/java/com/castelo/dispenca/DispencaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.castelo.dispenca;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DispencaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
