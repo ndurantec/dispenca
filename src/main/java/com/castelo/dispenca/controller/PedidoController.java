e89e3959b89f76cbb86b4fe83e8b45ad237390e1
AnthOnnii <anthoni.cardoso@escola.pr.gov.br>
2024-09-18 12:21:38 -0300

    socorro

--- src/main/java/com/castelo/dispenca/controller/PedidoController.java
+++ src/main/java/com/castelo/dispenca/controller/PedidoController.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -21,6 +22,7 @@ import com.castelo.dispenca.modelo.Pedido;
 import com.castelo.dispenca.repository.PedidoRepository;
 
 @RestController
+@CrossOrigin("*")
 @RequestMapping (value = "/pedido")
 public class PedidoController {
 
